018d9e8bbc570a67f1c541d80ba522179f0e8e73
Pablo Josué Stwolinsky Yat <111335751+pabstwo@users.noreply.github.com>
2026-09-08 01:42:35 -0600

    feat: agregar estructura inicial del analizador

---
+++ Analisis/AnalizadorLexico.cs
@@ -0,0 +1,31 @@
+﻿using AnalizadorLexicoCSharp.Modelos;
+
+namespace AnalizadorLexicoCSharp.Analisis
+{
+    public class AnalizadorLexico
+    {
+        private readonly TablaSimbolos tablaSimbolos;
+
+        public AnalizadorLexico()
+        {
+            tablaSimbolos = new TablaSimbolos();
+        }
+
+        public ResultadoAnalisis Analizar(
+            string codigoFuente)
+        {
+            tablaSimbolos.Limpiar();
+
+            ResultadoAnalisis resultado =
+                new ResultadoAnalisis();
+
+            // El reconocimiento de tokens se implementará
+            // después de estudiar autómatas en el curso.
+
+            resultado.Simbolos =
+                tablaSimbolos.ObtenerSimbolos();
+
+            return resultado;
+        }
+    }
+}
---
+++ Analisis/TablaSimbolos.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AnalizadorLexicoCSharp.Modelos;
+
+namespace AnalizadorLexicoCSharp.Analisis
+{
+    public class TablaSimbolos
+    {
+        private readonly Dictionary<string, Simbolo> simbolos;
+
+        public TablaSimbolos()
+        {
+            simbolos = new Dictionary<string, Simbolo>();
+        }
+
+        public void AgregarOActualizar(
+            string nombre,
+            int linea,
+            int columna,
+            string tipoDato = "No determinado")
+        {
+            if (simbolos.ContainsKey(nombre))
+            {
+                simbolos[nombre].RegistrarAparicion();
+            }
+            else
+            {
+                Simbolo nuevoSimbolo = new Simbolo(
+                    nombre,
+                    linea,
+                    columna,
+                    tipoDato);
+
+                simbolos.Add(nombre, nuevoSimbolo);
+            }
+        }
+        public List<Simbolo> ObtenerSimbolos()
+        {
+            return simbolos.Values.ToList();
+        }
+        public void Limpiar()
+        {
+            simbolos.Clear();
+        }
+    }
+}
